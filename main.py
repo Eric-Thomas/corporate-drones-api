56eebb64e776eb5e4693e6c8285ef8ddcdce4863
Eric <ethomasosu@gmail.com>
2022-11-20 00:25:18 -0500

    implement api

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import json
+
+import boto3
+
+S3_BUCKET = "corporate-drones-music-league"
+OBJECT_KEY = "round_results.json"
+
 def handler(event, context):
-    pass
+    return _get_json_body()
+
+
+def _get_json_body():
+    print("Getting json from s3...")
+    s3_client = boto3.client("s3")
+    return json.load(s3_client.get_object(Bucket=S3_BUCKET, Key=OBJECT_KEY)["Body"])
+
+if __name__ == '__main__':
+    _get_json_body()
