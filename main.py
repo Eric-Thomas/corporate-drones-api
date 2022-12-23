b184690f6b33a752ba33249f5f964cbb035a4a0a
Eric Thomas <ethomasosu@gmail.com>
2022-12-23 12:47:33 -0500

    change path to queryparam

--- main.py
+++ main.py
@@ -7,23 +7,29 @@ DICTATOR_LEAGUE_OBJECT_KEY = "dictator_league.json"
 CORPORATE_DRONES_OBJECT_KEY = "corporate_drones.json"
 
 def handler(event, context):
-    path = event.get("path")
-    resp = {}
-    print(f"{path} endpoint invoked")
-    if path == "/all" or path ==  "/":
+    print(event)
+    # Default is to return all leagues
+    league = "all"
+    query_string_params = event.get("queryStringParameters")
+    if  query_string_params:
+        league = query_string_params.get("league", "all")
+
+    if league == "all":
         dictator_league_body = _get_json_body(DICTATOR_LEAGUE_OBJECT_KEY)
         corporate_drones_body = _get_json_body(CORPORATE_DRONES_OBJECT_KEY)
         # Combine both league rounds
         dictator_league_body.get("rounds").extend(corporate_drones_body.get("rounds"))
         resp = dictator_league_body
-    elif path == "/corporate-drones":
+    elif league == "corporate-drones":
         resp = _get_json_body(CORPORATE_DRONES_OBJECT_KEY)
-    elif path == "/dictator-league":
+    elif league == "dictator-league":
         resp = _get_json_body(DICTATOR_LEAGUE_OBJECT_KEY)
     else:
-        return {"error": f"path {path} not a valid path"}
+        return {"error": f"league {league} not a valid league"}
 
     return resp
+
+
 
 
 def _get_json_body(object_key):
@@ -32,6 +38,6 @@ def _get_json_body(object_key):
     return json.load(s3_client.get_object(Bucket=S3_BUCKET, Key=object_key)["Body"])
 
 if __name__ == '__main__':
-    event = {"path": "/dictator-league"}
+    event = {"league": "/dictator-league"}
     with open("test.json", "w") as outfile:
         outfile.write(json.dumps(handler(event, None)))
